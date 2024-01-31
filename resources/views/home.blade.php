2d5faefdec45dbb9dc1796276991a06aae1fd0fe
rajk5266 <kushwaharaj903@gmail.com>
2024-01-31 20:31:05 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    @include('common/header')
+</head>
+
+
+<body>
+@include('common/navbar')
+    <h1>Home page data</h1>
+    <h1>Home page data</h1>
+    <h1>Home page data</h1>
+    @include('common-layout')
+    <h1>Home page data</h1>
+    <h1>Home page data</h1>
+    <h1>Home page data</h1>
+    <h1>Home page data</h1>
+</body>
+</html>
